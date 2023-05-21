94433153238b29392036c5b5a518f49d00ad64f6
윤동현 <eatpaper1429@gmail.com>
2023-05-22 03:07:19 +0900

    신고일자 추가

--- src/main/java/com/app/citizen/domain/BirthDeathReportResidentDto.java
+++ src/main/java/com/app/citizen/domain/BirthDeathReportResidentDto.java
@@ -1,5 +1,8 @@
 package com.app.citizen.domain;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class BirthDeathReportResidentDto {
 
   public String birthDeathReportResidentQualificationCode;
@@ -12,11 +15,19 @@ public class BirthDeathReportResidentDto {
 
   public String birthDeathReportResidentEmail;
 
-  public BirthDeathReportResidentDto(String birthDeathReportResidentQualificationCode, String birthDeathReportResidentName, String birthDeathReportResidentRegistrationNumber, String birthDeathReportResidentPhoneNumber, String birthDeathReportResidentEmail) {
+  public LocalDateTime birthDeathReportDate;
+
+  public BirthDeathReportResidentDto(String birthDeathReportResidentQualificationCode, String birthDeathReportResidentName, String birthDeathReportResidentRegistrationNumber, String birthDeathReportResidentPhoneNumber, String birthDeathReportResidentEmail, LocalDateTime birthDeathReportDate) {
     this.birthDeathReportResidentQualificationCode = birthDeathReportResidentQualificationCode;
     this.birthDeathReportResidentName = birthDeathReportResidentName;
     this.birthDeathReportResidentRegistrationNumber = birthDeathReportResidentRegistrationNumber;
     this.birthDeathReportResidentPhoneNumber = birthDeathReportResidentPhoneNumber;
     this.birthDeathReportResidentEmail = birthDeathReportResidentEmail;
+    this.birthDeathReportDate = birthDeathReportDate;
+  }
+
+  public String getFormattedDate() {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy 년 MM 월 dd 일");
+    return birthDeathReportDate.format(formatter);
   }
 }
